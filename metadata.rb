64ab0a6fa4f6bdf8bb7bb1194524798dbe35bec1
Sebastian Gerau <gsebast@web.de>
2015-11-08 20:36:48 +0100

    Updated 'docker' cookbook dependency to version ~> 2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'gsebast@web.de'
 license          'All rights reserved'
 description      'Installs/Configures Docker components'
 long_description 'Installs/Configures docker-node'
-version          '0.1.0'
+version          '0.1.2'
 
-depends					 'docker', '>= 1.0.0'
+depends					 'docker', '~> 2.0'
