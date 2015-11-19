06cef4ddee535ab1aa4639b3641b66c8a05c7c64
Sebastian Gerau <gsebast@web.de>
2015-11-19 21:15:17 +0100

    Added additional test-kitchen setups, i.e. testing Docker latest and 1.8.3

---
+++ test/integration/default/serverspec/service_spec.rb
@@ -0,0 +1,17 @@
+require "serverspec"
+
+# required by serverspec
+set :backend, :exec
+
+
+describe "Docker Daemon" do
+
+  it "should be enabled" do
+    expect(service("docker")).to be_enabled
+  end
+
+  it "should be running" do
+    expect(service("docker")).to be_running
+  end
+
+end
