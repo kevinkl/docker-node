45b052bbe6ae6e65ae40024ba27562703668c15f
Sebastian Gerau <gsebast@web.de>
2015-11-19 21:17:58 +0100

    Bumped cookbook version to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'gsebast@web.de'
 license          'All rights reserved'
 description      'Installs/Configures Docker components'
 long_description 'Installs/Configures docker-node'
-version          '0.1.2'
+version          '0.1.3'
 
 depends					 'docker', '~> 2.0'
