5ef34841aaec6c30b38b0e2fffb75604833ea797
Sebastian Gerau <gsebast@web.de>
2016-01-08 15:43:00 +0100

    Updated long description

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Sebastian Gerau'
 maintainer_email 'gsebast@web.de'
 license 'Apache 2.0'
 description 'Installs/Configures Docker components'
-long_description 'Installs/Configures docker-node'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.4'
 
 depends 'docker', '~> 2.0'
