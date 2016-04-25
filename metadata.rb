65dbd46ca9712361875f40e80028092f7cd81b90
Sebastian Gerau <gsebast@web.de>
2016-04-25 22:08:37 +0200

    Bumped cookbook version to 0.1.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gsebast@web.de'
 license 'Apache 2.0'
 description 'Installs/Configures Docker components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.5'
+version '0.1.6'
 
 depends 'docker', '~> 2.0'
 depends 'apt-docker', '~> 0.3'
